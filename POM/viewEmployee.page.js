3381699f4760c0ae0638f6f53a7b12d472c5f6c3
sinchansmarttester-glitch <sinchan.smarttester@gmail.com>
2026-07-15 15:53:45 +0530

    new

--- POM/viewEmployee.page.js
+++ POM/viewEmployee.page.js
@@ -1,18 +1,16 @@
-import { expect } from "@playwright/test"
+import { expect } from '@playwright/test'
 
 class ViewEmployee {
     constructor(page) {
         this.viewEmployeeButton = page.locator("//a[text()='View Employee']")
         this.assertion = page.locator("//tr/descendant::td[contains(.,'James Bond')]")
-       
-        //this.del=page.getByRole('link', { name: 'Delete' }).nth(2)
-        this.del=page.locator("//td[text()='James Bond']/following::a[contains(.,'Delete')]")
+        this.empID = page.locator("//td[contains(.,'Bond')]/ancestor::tr/td[1]")
     }
+
     async viewEmployee() {
         await this.viewEmployeeButton.click()
         await expect(this.assertion).toContainText("James Bond")
-        await this.del.click()
-    
+        return await this.empID.innerText()
     }
 }
-export default ViewEmployee 
+export default ViewEmployee
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,20 +1,35 @@
 import { test, expect } from '../Custom Fixure/loginFixure.js'
 import AddEmployee from '../POM/addEmployee.page.js'
+import AssignProject from '../POM/assignProject.page.js'
 import LogOut from '../POM/logout.page.js'
 import ViewEmployee from '../POM/viewEmployee.page.js'
 import { readExcel } from '../utils/excelUtility.js'
 
 test('Login Test', async ({ page, loginFixture }) => {
   await expect(page).toHaveURL('http://49.249.29.4:8081/TestServer/Build/Employee_Management_System//aloginwel.php')
-  const employeeData=readExcel('employees.xlsx', 'Employees')
+  const employeeData = readExcel('employees.xlsx', 'Employees')
   const employee = employeeData[0]
   const addEmp = new AddEmployee(page)
   const viewEmp = new ViewEmployee(page)
-  const logout=new LogOut(page)
-  await addEmp.addEmployee(employee.firstname,employee.lastname,employee.emailid,
-    employee.birthday,employee.phoneNum,employee.NID,employee.address,
-    employee.department,employee.degree,employee.salary)
-   await viewEmp.viewEmployee()
-   await page.waitForTimeout(5000)
-   await logout.logOut()
+  const assignProject = new AssignProject(page)
+  const logout = new LogOut(page)
+
+  await addEmp.addEmployee(
+    employee.firstname,
+    employee.lastname,
+    employee.emailid,
+    employee.birthday,
+    employee.phoneNum,
+    employee.NID,
+    employee.address,
+    employee.department,
+    employee.degree,
+    employee.salary
+  )
+
+  const employeeId = await viewEmp.viewEmployee()
+  await assignProject.assignProject(employeeId, 'Project Alpha', '2026-07-20')
+
+  await page.waitForTimeout(5000)
+  await logout.logOut()
 })
